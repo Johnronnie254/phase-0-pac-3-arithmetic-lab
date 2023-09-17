0227d67b3f76f2f6cb45dbbcbaf894ca2308f436
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-09-17 09:34:16 +0300

    arithmetic

---
+++ index.js
@@ -0,0 +1,51 @@
+function makeInt(string){
+   return parseInt(string,10);
+}
+console.log(makeInt("10"))
+//test 2
+function preserveDecimal(string){
+    return parseFloat(string,10);
+}
+console.log(preserveDecimal("oya"),10)
+
+//third test
+
+  function add(a,b){
+    return (a+b)
+  }
+  console.log(add(5,8))
+
+  function add(a,b){
+    return (a+b)
+  }
+  console.log(add(5,8))
+
+  function subtract(a,b){
+    return (a-b)
+  }
+  console.log(subtract(10,5))
+
+  function multiply(a,b){
+    return (a*b)
+  }
+  console.log(multiply(10,5))
+
+  function divide(a,b){
+    return (a/b)
+  }
+  console.log(divide(5,8))
+
+  //increment and decrement
+
+
+function increment(n) {
+    return n +1;
+    }
+  console.log(increment(6))
+
+function decrement(num){
+      return num -1;
+      }
+console.log(decrement(8))
+
+ 
